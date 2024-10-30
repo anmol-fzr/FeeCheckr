d43d4f3d6f5397d21d6218173a9f4da0d7822e8e
Anmol <ainsa2279@gmail.com>
2024-10-30 19:37:05 +0530

    ✨ Added Global Settings via FireBase for form opening

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,40 @@
 import { Chart } from "@/components/dashboard/Chart";
+import { ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+import { fbRealTimeDB } from "@/config";
+import { Alert, AlertTitle, AlertDescription } from "@/components";
 
 const Users = () => {
-  console.log("Users");
+  const [settings, setSettings] = useState<Record<string, boolean>>({});
+
+  useEffect(() => {
+    const settingsRef = ref(fbRealTimeDB);
+    onValue(settingsRef, (snapshot) => {
+      const data = snapshot.val();
+      setSettings(data);
+    });
+  }, []);
+
   return (
-    <div className=" grid grid-cols-4 gap-12 w-full">
-      <Chart />
-      <Chart />
-      <Chart />
-      <Chart />
-    </div>
+    <>
+      <Alert
+        variant="caution"
+        //className="w-full flex items-end justify-between gap-6 flex-wrap animate-in fade-in"
+      >
+        <div>
+          <AlertTitle> 📢 Attention !</AlertTitle>
+          <AlertDescription>
+            <p>Fee forms are Open, student can fill forms.</p>
+          </AlertDescription>
+        </div>
+      </Alert>
+      <div className=" grid grid-cols-4 gap-12 w-full">
+        <Chart />
+        <Chart />
+        <Chart />
+        <Chart />
+      </div>
+    </>
   );
 };
 
